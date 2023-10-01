bd3a21cbbc2d31a124699a07b0a0658bd11e307b
hamza-25 <me677499@gmail.com>
2023-10-01 18:43:18 +0000

    fix betty

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,25 +4,24 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 /**
- *
- *
+ * infinite_while - funtction that ran infinity
+ * Return: int 0 success
 */
 int infinite_while(void)
 {
 	while (1)
-		{
-			sleep(1);
-		}
+	{
+		sleep(1);
+	}
 	return (0);
 }
 
 /**
- *
- *
- *
- * */
+ * main - entry point for printing pid
+ * Return: int 0 success
+*/
 
-int main()
+int main(void)
 {
 	int i = 0;
 	pid_t pid;
