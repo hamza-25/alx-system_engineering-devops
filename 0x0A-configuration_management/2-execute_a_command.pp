ef0c91a331870cc191c686d3454a5fa080d05166
hamza-25 <me677499@gmail.com>
2023-11-24 20:42:12 +0000

    fix bugs

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
 # create a manifest that kills a process named killmenow.
-exec{'kill_process':
-command     => 'pkill -f killmenow',
+
+exec { 'kill_process':
+command  => 'pkill -f killmenow',
 provider => 'shell'
 }
