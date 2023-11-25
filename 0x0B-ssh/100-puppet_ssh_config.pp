a6fabfbebdc575aefff6650c66acf009faa69085
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-11-25 20:01:28 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,12 +5,10 @@ file_line { 'no pass':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
-  replace => true,
 }
 
 file_line { 'identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
-  replace => true,
 }
