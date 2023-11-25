a1bac81032cd764873372386ff01035cf7550b8e
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-11-25 19:57:06 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,32 @@
 # Puppet to make changes to our configuration file
+#include stdlib
+#
+#file_line { 'No password':
+#ensure  => present,
+#path    => '/etc/ssh/ssh_config',
+#line    => 'PasswordAuthentication no',
+#replace => True,
+#}
+#
+#file_line { 'Identity file':
+#ensure  => present,
+#path    => '/etc/ssh/ssh_config',
+#line    => 'IdentityFile ~/.ssh/school',
+#replace => True,
+#}
+# Setting up my client config file
 include stdlib
 
-file_line { 'No password':
-ensure  => present,
-path    => '/etc/ssh/ssh_config',
-line    => 'PasswordAuthentication no',
-replace => True,
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Identity file':
-ensure  => present,
-path    => '/etc/ssh/ssh_config',
-line    => 'IdentityFile ~/.ssh/school',
-replace => True,
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
