7ed40ed3ee7a8ddc4d121e1e72c837b2c7bbb47b
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-11-28 22:58:57 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,5 @@
 # installing nginx using puppet
-# Update package lists
-exec {'update_apt':
-  command => 'sudo apt-get -y update',
-  path    => '/usr/bin',
-}
-
-# Install Nginx
-package {'nginx':
-  ensure => present,
-  before => File['/var/www/html/index.nginx-debian.html'],
-}
-
-# Create Hello World HTML file
-file {'/var/www/html/index.nginx-debian.html':
-  ensure  => present,
-  content => 'Hello World!',
-  notify  => Exec['nginx_restart'],
-}
-
-# Configure Nginx to include redirection rule
-file {'/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => template('module_name/default.erb'), # Use an ERB template for better structure
-  notify  => Exec['nginx_restart'],
-}
-
-# Restart Nginx
-exec {'nginx_restart':
-  command => 'sudo service nginx restart',
-  refreshonly => true,
-}
-
-
-exec {'nginx_restart':
-command     => 'sudo service nginx restart',
-refreshonly => true,
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
