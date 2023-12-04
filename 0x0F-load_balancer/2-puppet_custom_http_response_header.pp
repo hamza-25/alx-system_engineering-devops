2594f894abce435187033561fd8f080941aecab5
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-12-04 21:05:00 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,8 @@
 # automate the task of creating a custom HTTP header response
-exec {'update':
-provider => shell,
-command  => 'sudo apt -y update',
-before   => Exec['install_nginx'],
-}
-
-exec {'install_nginx':
-provider => shell,
-command  => 'sudo apt -y install nginx',
-before   => Exec['add_header'],
-}
-
-exec { 'add_header':
-  provider    => shell,
-  environment => ["MYNAME=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\default;/include \/etc\/nginx\/sites-enabled\/\default;\n\tadd_header X-Served-By \"$MYNAME\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart_nginx'],
-}
-
-exec { 'restart_nginx':
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
-  command  => 'sudo service nginx restart',
 }
