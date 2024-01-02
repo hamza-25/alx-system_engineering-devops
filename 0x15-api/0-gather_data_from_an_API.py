7550c2321b601fa4cbdf79c8f40dd80195a80212
hamza-25 <me677499@gmail.com>
2024-01-02 19:07:43 +0100

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,10 +4,8 @@ using this REST API, for a given employee ID, returns information about
 ARGS:
     fisrt arg: is the id of the employee
 """
-
 import requests
-from sys import argv
-import json
+import sys
 
 if __name__ == "__main__":
     response_user = requests.get('https://jsonplaceholder.typicode.com/users')
@@ -15,14 +13,14 @@ if __name__ == "__main__":
     if response_user.status_code == 200 and response_todo.status_code == 200:
         response_user = response_user.json()
         for user in response_user:
-            if user['id'] == int(argv[1]):
+            if user['id'] == int(sys.argv[1]):
                 emp_name = user['name']
         completed = 0
         total_task = 0
         title_completed_task = []
         response_todo = response_todo.json()
         for do in response_todo:
-            if do['userId'] == int(argv[1]):
+            if do['userId'] == int(sys.argv[1]):
                 if do['completed'] is True:
                     title_completed_task.append(do['title'])
                     completed += 1
