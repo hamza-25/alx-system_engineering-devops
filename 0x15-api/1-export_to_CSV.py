6f28e264b8f5663668849ad460e358cd2275c9f5
hamza-25 <me677499@gmail.com>
2024-01-02 19:55:57 +0100

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
-import json
-
 """
 using this REST API, for a given employee ID, returns information about
 ARGS:
     fisrt arg: is the id of the employee
 """
+import csv
+import requests
+import sys
 
-response_user = requests.get('https://jsonplaceholder.typicode.com/users')
-response_todo = requests.get('https://jsonplaceholder.typicode.com/todos')
-
-if response_user.status_code == 200 and response_todo.status_code == 200:
-    response_user = response_user.json()
-    for user in response_user:
-        if user['id'] == int(argv[1]):
-            emp_name = user['name']
-
-    completed = 0
-    total_task = 0
-    title_completed_task = []
-    response_todo = response_todo.json()
-    for do in response_todo:
-        if do['userId'] == int(argv[1]):
-            if do['completed'] is True:
-                title_completed_task.append(do['title'])
-                completed += 1
-            total_task += 1
-
-    print(f'Employee {emp_name} is done with tasks({completed}/{total_task}):')
-    for title in title_completed_task:
-        print(f'\t{title}')
+if __name__ == "__main__":
+    response_user = requests.get('https://jsonplaceholder.typicode.com/users')
+    response_todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+    if response_user.status_code == 200 and response_todo.status_code == 200:
+        response_user = response_user.json()
+        for user in response_user:
+            if user['id'] == int(sys.argv[1]):
+                emp_name = user['name']
+                username = user['username']
+        completed = 0
+        total_task = 0
+        title_completed_task = []
+        all_tasks = []
+        response_todo = response_todo.json()
+        for do in response_todo:
+            if do['userId'] == int(sys.argv[1]):
+                if do['completed'] is True:
+                    title_completed_task.append(do['title'])
+                    completed += 1
+                all_tasks.append(do['title'])
+                total_task += 1
+        matrix_rows = []
+        for title in all_tasks:
+            if title in title_completed_task:
+                matrix_rows.append([sys.argv[1], username, "True", title])
+            else:
+                matrix_rows.append([sys.argv[1], username, "False", title])
+        with open('USER_ID.csv', "w", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(matrix_rows)
