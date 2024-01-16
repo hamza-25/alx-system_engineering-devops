a2df83ab0f2fc67479190ee9c0a2dc5b0bc46e34
hamza-25 <me677499@gmail.com>
2024-01-16 20:01:39 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""define module total subs"""
 import requests
 import sys
 
@@ -7,7 +8,9 @@ def number_of_subscribers(subreddit):
     """number of subscribers"""
     if subreddit is None or type(subreddit) is not str:
         return (0)
-    url = f'https://www.reddit.com/r/{sys.argv[1]}/about.json'
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url)
+    if response.status_code != 200:
+        return (0)
     response_json = response.json()
     return (response_json['data']['subscribers'])
