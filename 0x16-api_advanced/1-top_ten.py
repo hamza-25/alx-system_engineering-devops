0a1968cc086fd2e2758491a36a724e73b2ce3f4e
hamza-25 <me677499@gmail.com>
2024-01-16 20:37:10 +0100

    update

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define module print 10 title"""
+import requests
+
+
+def top_ten(subreddit):
+    """print title"""
+    if subreddit is None or type(subreddit) is not str:
+        return (0)
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+    headers = {'User-Agent': 'hamzaApp/1.0 by hamza-25'}
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    data = response.json().get('data')
+    for title in data['children']:
+        print(title.get('data').get('title'))
